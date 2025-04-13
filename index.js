4dc993e3a3c3ad481495e523700a32d874863f94
Rihaya22 <sakaido19@gmail.com>
2025-04-13 21:15:23 +0700

    Adding button active effect
    Finishing the project

---
+++ index.js
@@ -0,0 +1,41 @@
+const randomDice = [];
+var randomNumberOne;
+var randomNumberTwo;
+const headerText = document.querySelector('h1');
+const subText = document.querySelector('p');
+const diceOne = document.querySelectorAll('img')[0];
+const diceTwo = document.querySelectorAll('img')[1];
+const button = document.querySelector('button');
+
+randomDice.push("./images/dice-six-faces-one.png");
+randomDice.push("./images/dice-six-faces-two.png");
+randomDice.push("./images/dice-six-faces-three.png");
+randomDice.push("./images/dice-six-faces-four.png");
+randomDice.push("./images/dice-six-faces-five.png");
+randomDice.push("./images/dice-six-faces-six.png");
+
+function randomDiceGenerator() {
+	randomNumberOne = Math.floor(Math.random() * 7);
+	randomNumberTwo = Math.floor(Math.random() * 7);
+	diceOne.setAttribute("src", randomDice[randomNumberOne]);
+	diceTwo.setAttribute("src", randomDice[randomNumberTwo]);
+	result();
+}
+
+function result() {
+	if(randomNumberOne > randomNumberTwo) {
+		headerText.innerHTML = "Player 1 Win";
+		subText.innerHTML = "Congratulations!";
+	} else if(randomNumberOne == randomNumberTwo) {
+			headerText.innerHTML = "Draw"
+			subText.innerHTML = "Try again!"
+	} else {
+			headerText.innerHTML = "Player 2 Win";
+			subText.innerHTML = "Congratulations";
+	}
+}
+
+button.onclick = randomDiceGenerator;
+
+
+
